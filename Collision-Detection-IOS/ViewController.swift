c98cc5c1635e50e3180ebc66ea12f53bda06ab7a
yjs1213 <jidbyoo@gmail.com>
2021-12-01 22:07:37 +0900

    Label 아웃렉 변수 추가

--- Collision-Detection-IOS/ViewController.swift
+++ Collision-Detection-IOS/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblxVal: UILabel!
+    @IBOutlet var lblyVal: UILabel!
+    @IBOutlet var lblzVal: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
