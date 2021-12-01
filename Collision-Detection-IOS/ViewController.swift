3cb3fc5d9d14cc33719a8c20d1236204fb5e0703
yjs1213 <jidbyoo@gmail.com>
2021-12-01 22:54:35 +0900

    충돌이 감지되면 소리 출력

--- Collision-Detection-IOS/ViewController.swift
+++ Collision-Detection-IOS/ViewController.swift
@@ -7,16 +7,30 @@
 
 import UIKit
 import CoreMotion
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet var lblxVal: UILabel!
     @IBOutlet var lblyVal: UILabel!
     @IBOutlet var lblzVal: UILabel!
+    @IBOutlet var lblRecentx: UILabel!
+    @IBOutlet var lblRecenty: UILabel!
+    @IBOutlet var lblRecentz: UILabel!
     
     let motion = CMMotionManager()
+    var audioPlayer : AVAudioPlayer!
+    var audioFile : URL!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        audioFile = Bundle.main.url(forResource: "Blop Sound", withExtension: "mp3")
+        initPlay()
+        
+        lblRecentx.text = "0"
+        lblRecenty.text = "0"
+        lblRecentz.text = "0"
         
         if self.motion.isAccelerometerAvailable{
             self.motion.accelerometerUpdateInterval = 1.0 / 60.0
@@ -28,17 +42,15 @@ class ViewController: UIViewController {
                     let val_y = data.acceleration.y
                     let val_z = data.acceleration.z
                     
-                    if val_x > 8 {
-                        print("충돌 감지 : X가 8 넘음")
-                        print("X : \(val_x)")
-                    }
-                    else if val_y > 8 {
-                        print("충돌 감지 : Y가 8 넘음")
-                        print("Y : \(val_y)")
-                    }
-                    else if val_z > 8 {
-                        print("충돌 감지 : Z가 8 넘음")
-                        print("Z : \(val_z)")
+                    if val_x > 8 || val_y > 8 || val_z > 8{
+                        print("충돌 감지")
+                        print("X : \(val_x) Y : \(val_y) Z : \(val_z)")
+                        
+                        self.lblRecentx.text = String(format: "%.2f",val_x)
+                        self.lblRecenty.text = String(format: "%.2f",val_y)
+                        self.lblRecentz.text = String(format: "%.2f",val_z)
+                        
+                        self.audioPlayer.play()
                     }
                     
                     self.lblxVal.text = String(format: "%.2f", val_x)
@@ -49,7 +61,14 @@ class ViewController: UIViewController {
             RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
         }
     }
-
+    func initPlay(){
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFile)
+        } catch let error as NSError{
+            print("Error Initial Play")
+        }
+        audioPlayer.prepareToPlay()
+    }
 
 }
 
